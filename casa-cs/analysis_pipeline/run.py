8f6d88f33105ca8c92bce8f4815e72ab43615a11
Jon <jon@localhost.localdomain>
2018-05-23 04:03:33 -0400

    hacking

--- casa-cs/analysis_pipeline/run.py
+++ casa-cs/analysis_pipeline/run.py
@@ -26,7 +26,26 @@ def create_csv():
             map = np.reshape(image.getchunk(), dimensions)[0:dimensions[0], 0:dimensions[1]] #weird but this line is needed as is or weird exceptions happen
             np.savetxt(os.path.join(outImgFolder, newName), map, delimiter=", ")
             image.close()
+    print("done creating csv images")
+
+
+def convolve():
+    print("nothing")
+
+def createTestImage():
+    dimensions = (64, 64)
+    image = _casac.image().newimage(infile="./casa_img/sun.flare00_64.64/sun_center_event_64_clean.psf")
+    map = np.reshape(image.getchunk(), dimensions)[0:dimensions[0], 0:dimensions[1]]  # weird but this line is needed as is or weird exceptions happen
+
+    test_map = np.zeros(dimensions)
+    for i in range(dimensions[0]):
+        for j in range(dimensions[1]):
+            if i -24 >= 0 and j-13 >=0:
+                test_map[i-24, j-13] = map[i, j]
+
+    np.savetxt("test_psf", test_map, delimiter=", ")
 
 create_csv()
 
-
+createTestImage()
+#
